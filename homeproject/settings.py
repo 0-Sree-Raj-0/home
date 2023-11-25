5d36b0eeb4c8c68afd4438fd0a1248f1ffda82fd
0-Sree-Raj-0 <150782014+0-Sree-Raj-0@users.noreply.github.com>
2023-11-25 16:33:47 +0530

    Update settings.py

--- homeproject/settings.py
+++ homeproject/settings.py
@@ -117,12 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'homeapp/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'homeapp/static'),  # Replace 'static' with your static files directory if different
-]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'staticfiles/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'homeapp/static'),  # Replace 'static' with your static files directory if different
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
