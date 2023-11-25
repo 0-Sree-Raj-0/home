c2925f36d66da9d482d1cd4f1880c28d29d9b6d4
0-Sree-Raj-0 <150782014+0-Sree-Raj-0@users.noreply.github.com>
2023-11-25 16:21:47 +0530

    Update settings.py

--- homeproject/settings.py
+++ homeproject/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = 'homeapp/static/'
+STATIC_URL = 'homeapp/static/'
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'homeapp/static'),  # Replace 'static' with your static files directory if different
